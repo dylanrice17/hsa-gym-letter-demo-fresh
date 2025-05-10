c1e7e7222362d8531d00c8eb5ce422aff7134de5
Dylan Rice <dylanrice@dylans-air.mynetworksettings.com>
2025-05-10 16:55:29 -0400

    Add minimal App.js entry point for React app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>HSA Gym Letter Generator</h1>
+      <p>Your React app is working!</p>
+    </div>
+  );
+}
+
+export default App; 
